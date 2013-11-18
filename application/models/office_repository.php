654abf744a0d2b224342a98a969149e082c3a8e2
unknown <nogovern@gmail.com>
2013-11-18 17:55:55 +0900

    거래처 모델 추가 office_repository 파일 추가 거래처 입력 단위테스트 진행 doctrine2 를 쓰는것이 편할지???

---
+++ application/models/office_repository.php
@@ -0,0 +1,101 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * 
+ */
+
+class Office_repository extends CI_Model {
+	private $table_name;
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->db = $this->load->database('oracle', TRUE);
+
+		$this->table_name = "gs2_offices";
+		$this->table_name = strtoupper($this->table_name);		// oracle 테이블명은 대문자로
+	}
+
+	public function getMaxId() {
+		$sql = "SELECT MAX(ID) as ID FROM " . $this->table_name;
+		$rs = $this->db->query($sql);
+
+		$row = $rs->row();
+
+		return $row->ID;
+	}
+
+	public function add($object) {
+		if( !is_object($object))
+			trigger_error("객체 인스턴스만 가능함!");
+
+		$new_id = $this->newId();
+		$this->db->set("ID", $new_id);
+
+		$props = $object->getPropertiesList(); 
+		foreach( $props as $prop) {
+			// echo $prop;
+			if(!empty($object->{$prop})) {
+				$this->db->set(strtoupper($prop), $object->$prop);
+			}
+		}
+
+		$this->db->insert($this->table_name);
+
+		return $new_id;
+	}
+
+	////////////////////////////////
+	// 공통으로 사용할 수 있을듯 
+	////////////////////////////////
+	public function newId() {
+		$sql = "select max(ID) as new_id from " . $this->table_name;
+		$query = $this->db->query($sql);
+
+		return ($query->num_rows) ? $query->row()->NEW_ID + 1 : 1;
+	}
+
+	public function truncate() {
+		$sql = "truncate table " . $this->table_name;
+		$this->db->query($sql);
+
+		echo '\n {$this->table_name} is truncated.';
+	}
+
+}
+
+class Office {
+	var $id;
+	var $code;
+	var $name;
+	var $is_inventory;
+	var $inventory_id;
+	var $tel;
+	var $address;
+	var $memo;
+	var $status;
+
+	private $props = array();
+
+	public function __construct($id = '')
+	{
+		$repl = new ReflectionClass($this);
+		$props = $repl->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED);
+		
+		foreach($props as $obj) {
+			array_push($this->props, $obj->name);
+		}
+	}
+
+	public function set($val = '') {
+
+	}
+
+	public function _set($val = '') {
+		print_r($this->props);
+	}
+
+	public function getPropertiesList() {
+		return $this->props;
+	}
+}
--- tests/users/UserTest.php
+++ tests/users/UserTest.php
@@ -43,6 +43,38 @@ class UserTest extends PHPUnit_Framework_TestCase {
 
 	public function testDoctrine2()
 	{
-		$this->CI->load->library('doctrine');
+		// $this->CI->load->library('doctrine');
 	}
+
+	/**
+	 * 사무소 입력 테스트
+	 */
+	public function testOfficeSingleAdd()
+	{
+		$this->CI->load->model('office_repository');
+		$office_repository = $this->CI->office_repository;
+
+		$office1 = new Office();
+		$office1->name = "본사";
+		$office1->is_inventory = "N";
+
+		$office_id = $office_repository->add($office1);
+
+		// offices 테이블의 id 최대값이 입력 시 리턴 됨 id 와 같은지 확인
+		$max_id = $office_repository->getMaxId();
+		$this->assertEquals($max_id, $office_id);
+
+		// 테스트 후 입력데이터 초기화 (truncate???)
+		// 
+		// $office_repository->truncate();
+	}
+
+	/**
+	 * 거래처 입력 테스트
+	 */
+	public function testCustomerAdd()
+	{
+		
+	}
+
 }
