bfb2b530c95c2d4854135770d459a53b32044028
unknown <nogovern@gmail.com>
2013-11-19 16:33:20 +0900

    bootstrap form 요소 수정 한행에서 input tag, button tag 의 정렬을 맞출려면 div.form-horizontal 로 감싸면 된다.

--- application/views/user_add_form.php
+++ application/views/user_add_form.php
@@ -9,33 +9,40 @@
             <label>사용자 구분</label>
             <label class="radio">
               <input type="radio" name="gubun" id="gubun_1" checked>
-              시스넷관리자
+              시스넷 서비스
             </label>
             <label class="radio">
               <input type="radio" name="gubun" id="gubun_2">
-              GS25유저
+              GS25 유저
+            </label>
+            <label class="radio">
+              <input type="radio" name="gubun" id="gubun_3">
+              납품처 유저
             </label>
 
             <label>사용자 ID</label>
-            <input type="text" name="username" placeholder="ID를 입력하세요...">
+            <div class="form-horizontal">
+              <input type="text" name="username" placeholder="ID를 입력하세요...">
+              <button type="button" class="btn btn-warning">ID 중복확인</button>
+            </div>
 
             <label>이름</label>
-            <input type="text" name="name" placeholder="성함을 입력하세요...">
+            <input type="text" name="name" placeholder="(필수)성함을 입력하세요...">
 
             <label>패스워드</label>
             <input type="password" name="password" placeholder="패스워드를 입력하세요...">
 
             <label>패스워드 확인</label>
             <input type="password" name="re_password" placeholder="패스워드를 다시 입력하세요...">
+
+            <label>연락처</label>
+            <input type="text" name="tel" placeholder="전화번호를 입력하세요...">
           </fieldset>
 
-          <p>
+          <p class="form-actions">
             <button class="btn btn-primary" type="button">입력완료</button>
           </p>
 
-          <p class="submit">
-            <input type="submit" vlaue="Send md the activation link">
-          </p>
         </form>
 
         </div>
