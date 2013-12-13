8f7a2fda68407074d8d39184cffb7c281ad8047a
unknown <nogovern@gmail.com>
2013-12-13 13:26:14 +0900

    Store 모델 추가 모델 파일 line ending 을 unix 로 변경

--- application/models/store_m.php
+++ application/models/store_m.php
@@ -4,14 +4,14 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
 * 
 */
-class Stock_m extends MY_Model
+class Store_m extends MY_Model
 {
 	
 	function __construct()
 	{
 		parent::__construct();
 
-		$this->setTableName('gs2_invnetory_part');
-		$this->setEntityName('Stock');
+		$this->setTableName('gs2_stores');
+		$this->setEntityName('Store');
 	}
 }
