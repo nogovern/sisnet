9e49808f031c12c8743ef1e7c848ad59b3575f91
unknown <nogovern@gmail.com>
2013-12-18 15:50:01 +0900

    전역 상수 추가

--- application/config/constants.php
+++ application/config/constants.php
@@ -39,3 +39,18 @@ define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
+
+define('GS2_LOCATION_TYPE_OFFICE', 	'O');			// 사무소
+define('GS2_LOCATION_TYPE_COMPANY', 'C');			// 거래처
+define('GS2_LOCATION_TYPE_STORE', 	'S');			// 점포
+
+define('GS2_OPERATION_STATUS_ENTER', 	100);			// 입고
+define('GS2_OPERATION_STATUS_INSTALL', 	200);			// 설치
+define('GS2_OPERATION_STATUS_CHANGE', 	300);			// 교체
+define('GS2_OPERATION_STATUS_WITHDRAW',	400);			// 철수
+define('GS2_OPERATION_STATUS_REPAIR', 	500);			// 장비 수리
+define('GS2_OPERATION_STATUS_DESTROY', 	510);			// 장비 폐기
+define('GS2_OPERATION_STATUS_MOVE', 	520);			// 장비 이동
+define('GS2_OPERATION_STATUS_TRANS', 	530);			// 장비 이관
+define('GS2_OPERATION_STATUS_ETC', 		900);			// 상태 변경
+
