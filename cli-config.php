5d722792edf317aa62de4248e4dfc820fb50f172
광희 장 <nogovern@gwanghui-jang-ui-MacBook-Pro.local>
2014-01-03 01:23:22 +0900

    add file for doctrine cli setting

---
+++ cli-config.php
@@ -0,0 +1,27 @@
+<?php
+// bootstrap.php
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once "vendor/autoload.php";
+
+// Create a simple "default" Doctrine ORM configuration for Annotations
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/application/models/Entity"), $isDevMode);
+// or if you prefer yaml or XML
+//$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
+//$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
+
+// database configuration parameters
+$conn = array(
+    'driver' => 'pdo_mysql',
+    'host'     => '127.0.0.1',
+    'dbname'   => 'sisnet',
+    'user'     => 'root',
+    'password' => ''
+);
+
+// obtaining the entity manager
+$entityManager = EntityManager::create($conn, $config);
+
+return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
