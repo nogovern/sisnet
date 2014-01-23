53c8397b1060db0d41309a26ea02b91d1bd88a84
unknown <nogovern@gmail.com>
2014-01-23 17:59:09 +0900

    상태변경 요청서 파일 추가

--- application/controllers/work/change.php
+++ application/controllers/work/change.php
@@ -28,7 +28,27 @@ class Change extends CI_Controller
 	}
 
 	public function register() {
-		echo '작업중';
+		$data['title'] = '장비 상태변경 변경 - 등록';
+		$data['current'] = 'page-changer';
+
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+
+		// == 테스트 ===
+		// 철수 업무 목록 중... 상태가 3 이상인 것만
+		$em = $this->work_model->getEntityManager();
+		$qb = $em->createQueryBuilder();
+		$qb->select('w')
+			->from('\Entity\Operation', 'w')
+			->where('w.type >= 300')
+			->andWhere('w.type < 400')
+			->andWhere('w.status >= 3')
+			->orderBy('w.id', 'DESC');
+
+		$rows = $qb->getQuery()->getResult();
+		$data['rows'] = $rows;
+		
+		$this->load->view('work/work_change_request_form', $data);
 	}
 
 
