fac1e4797759cbd0c41ecee219782be609d41fc0
unknown <nogovern@gmail.com>
2014-01-28 11:10:06 +0900

    상태변경 관련 테이블명 - gs2_operation_targets 로 변경함

--- application/models/Entity/OperationTarget.php
+++ application/models/Entity/OperationTarget.php
@@ -3,31 +3,48 @@ namespace Entity;
 
 /**
  * @Entity
- * @Table(name="GS2_OPERATION_EXTRAS")
+ * @Table(name="GS2_OPERATION_TARGETS")
  */
-class OperationExtra
+class OperationTarget
 {
 	/**
 	 * @Id
 	 * @Column(type="integer", nullable=false)
 	 * @GeneratedValue(strategy="AUTO")
-	 * @SequenceGenerator(sequenceName="gs2_op_extra_seq")
+	 * @SequenceGenerator(sequenceName="gs2_op_target_seq")
 	 */
 	protected $id;
 
 	/**
-	 * @ManyToOne(targetEntity="Operation", inversedBy="extras")
+	 * @OneToOne(targetEntity="Operation")
 	 * @JoinColumn(name="operation_id", referencedColumnName="id")
 	 */
 	protected $operation;
+
+	/**
+	 * @ManyToOne(targetEntity="Operation", inversedBy="targets")
+	 * @JoinColumn(name="target_operation_id", referencedColumnName="id")
+	 */
+	protected $target;
 
 	/** @Column(type="string", length=1) */
 	protected $is_complete = 'N';
 
 	//=====================================================
 	
+	public function __construct($op, $target) {
+		$this->operation = $op;
+		$this->target = $target;
+	}
+	
+	// 이 업무 작업 ID
 	public function setOperation($op) {
 		$this->operation = $op;
+	}
+
+	// 대상 작업 ID
+	public function setTarget($op) {
+		$this->target = $op;
 	}
 
 	public function setCompleteFlag($flag) {
