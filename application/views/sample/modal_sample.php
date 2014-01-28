4b265f9ac1258fb12672acdd8ba49667ea717d2e
unknown <nogovern@gmail.com>
2014-01-28 15:40:51 +0900

    페이지에 modal-content 까지 틀만 있고 안에 내용은 ajax load 하여 보여주기 위한 샘플

---
+++ application/views/sample/modal_sample.php
@@ -0,0 +1,15 @@
+<!-- 철수완료 작업 리스트 modal dialog -->
+
+<div class="modal-header">
+  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+  <h4 class="modal-title">철수완료 작업 리스트</h4>
+</div>
+<div class="modal-body">
+  <h1>불러 왔어요</h1>
+</div>
+<div class="modal-footer">
+  <button type="button" class="btn btn-primary">저장</button>
+  <button type="button" class="btn btn-default" data-dismiss="modal">닫기</button>
+</div>
+
+
