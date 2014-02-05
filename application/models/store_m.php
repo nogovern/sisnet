a08bd86d8590e49ef98eb3728ccce47ba9e6ff76
광희 장 <nogovern@gwanghui-jang-ui-MacBook-Pro.local>
2014-02-06 01:25:24 +0900

    휴점 장비용 모델 - 점포 모델안에 정의함 원격작업이 안되어 테스트 없이 코딩해봄

--- application/models/store_m.php
+++ application/models/store_m.php
@@ -68,11 +68,102 @@ class Store_m extends MY_Model
 		return $store;
 	}
 
-	// 수정
-	public function update($id, $data) {
-		$func_map = array(
-			'code'	=>	'setCode',
-			'has_postbox'	=> 'setPostbox'
-		);
+	// 점포 정보 수정
+	public function update($id, $data, $do_flush = FALSE) {
+		$store = $this->get($id);
+
+		// 매직 메소드 사용하여 단순화 함
+		foreach($data as $key => $val) {
+			if($key == 'id') {
+				continue;
+			}
+
+			$this->$key = $val;
+		}
+
+		$this->em->persist($store);
+
+		if($do_flush) {
+			$this->em->flush();
+		}
+
+		return $store;
+	}
+
+	// 휴점 장비 리스트 (기본적으로 operation 으로 검색)
+	public function getRestPartList($op) {
+		$repo = $this->em->getRpository('Entity\RestPart');
+		$items = $repo->findBy(array('operation' => $op, 'is_install' => 'N'));
+
+		return $items;
+	}
+
+
+	// 휴점 장비 리스트 - 점포 id 로 검색
+	public function getRestPartListByStore($store_id) {
+		$repo = $this->em->getRpository('Entity\RestPart');
+
+		$store = $this->em->getReference('Entity\Store', $store_id);
+		$items = $repo->findBy(array('store' => $store));
+
+	}
+
+	// 휴점 장비 등록(추가)
+	// 필수 데이터 - id, store_id, part_id, qty
+	public function addRestPart($data, $do_flush = FALSE) {
+
+		$item = new Entity\RestPart;
+
+		$op = $this->em->getReference('Entity\Operation', $data['id']);
+		$item->operation = $op;
+
+		$part = $this->em->getReference('Entity\Part', $data['part_id']);
+		$item->part = $part;
+
+		$store = $this->em->getReference('Entity\Store', $data['store_id']);
+		$item->store = $store;
+
+		$item->qty = $data['qty'];		// 수량
+		$item->is_install = 'N';		// 휴점 장비 설치 flag
+
+		$item->setDateRegister();
+		
+		$this->em->persist($item);
+
+		if($do_flush) {
+			$this->em->flush();
+		}
+
+		return $item;
+	}
+
+	// 휴점 장비 삭제
+	public function removeRestPart($item_id) {
+		;
+	}
+
+	// 철수 가능한 점포 리스트 (status = 1)
+	// 정상 운영중인 곳
+	public function getListInNormal() {
+		return FALSE;
+	}
+
+	// 휴점 설치 가능한 점포 리스트
+	public function getListInRest() {
+		return FALSE;
+	}
+
+	// 점포 상태 변경
+	public function setStatus($store_id, $status, $do_flush = FALSE) {
+		$store = $this->get($store_id);
+
+		$store->setStatus($status);
+
+		$this->em->persist($store);
+
+		if($do_flush) {
+			$this->em->flush();
+		}
 	}
 }
+
