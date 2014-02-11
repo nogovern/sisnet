0c43d965e8800b478db4f5ab77ca2b25e0aa4be4
광희 장 <nogovern@gwanghui-jang-ui-MacBook-Pro.local>
2014-02-12 02:36:42 +0900

    work for #39 - 설치 요청서에 점포 검색 modal 읽어서 붙이도록 적용

--- application/views/work_install_add_form.php
+++ application/views/work_install_add_form.php
@@ -119,6 +119,9 @@ $this->view('layout/navbar');
 
       </div>
     </div><!-- start of div.container -->
+<?php
+$this->view('common/modal_search_store');
+?>
 
     <!-- jquery form validation -->
     <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
@@ -222,73 +225,6 @@ $this->view('layout/navbar');
       });
     }
     </script>
-
-<!-- 점포 검색 modal dialog -->
-<div class="modal fade" id="modal_store_search_result" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title">점포 검색 리스트</h4>
-      </div>
-      <div class="modal-body">
-        <h5>점포 검색 결과 : <span id="cnt_result"></span> 건</h5>
-        <table class="table table-hover">
-          <thead>
-            <tr>
-              <th></th>
-              <th>점포명</th>
-              <th>점주</th>
-              <th>주소</th>
-              <th>연락처</th>
-              <th>상태</th>
-              <th>선택</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div class="modal-footer">
-        <button id="btn_modal_store_register" type="button" class="btn btn-primary">신규 등록</button>
-        <button type="button" class="btn btn-default" data-dismiss="modal">닫기</button>
-      </div>
-    </div>
-  </div>
-</div><!-- /.modal -->
-
-<?php
-// $this->view('common/modla_store_info');
-?>
-
-<script type="text/javascript">
-  $(document).ready(function(){
-    $("#btn_modal_store_register").click(function(e){
-      $("#modal_store_search_result").modal("hide");
-      callback_store_register();
-    });
-  });
-
-  $(document).on('click', '.select_me', function(e){
-    // 상점 id, name
-    var store_id = $(this).closest('tr').find('td:eq(0)').text();
-    var store_name = $(this).closest('tr').find('td:eq(1)').text();
-
-    // callback 함수 사용하여 부모창 element 에 설정
-    callback_store_info(store_id, store_name);
-
-    $("#modal_store_search_result").modal("hide");
-  });
-</script>
 
 <?php
 $this->view('layout/footer');
