28e23fd8f860045bda5037fde33845505fd9b53a
unknown <nogovern@gmail.com>
2014-02-25 17:57:50 +0900

    work for #4 - 폐기장비 승인 리스트 목록 execel 다운로드 테스트

--- application/controllers/myexcel.php
+++ application/controllers/myexcel.php
@@ -9,7 +9,73 @@ class Myexcel extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		echo "Excle main";
+	}
+
+	// 엑셀 파일 다운로드
+	public function download($objExcel, $save_filename) {
+		// redirect output to client browser
+		header("Pragma: public");
+		header("Expires: 0");
+		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+		header("Content-Type: application/octet-stream");
+		header('Content-Type: application/vnd.ms-excel');
+		header('Content-Disposition: attachment;filename="' . $save_filename. '"');
+		header('Cache-Control: max-age=0');
+
+		$objWriter = PHPExcel_IOFactory::createWriter($objExcel, 'Excel2007');
+		$objWriter->setOffice2003Compatibility(true);
+		$objWriter->save('php://output');
+	}
+
+	// 폐기 대기 장비 목록
+	public function destroy() {
+		$objPHPExcel = new PHPExcel();
+
+		// Set properties
+		$objPHPExcel->getProperties()->setCreator("Sisnet Service");
+		$objPHPExcel->getProperties()->setLastModifiedBy("Sisnet Service");
+		$objPHPExcel->getProperties()->setTitle("페기장비 목록 - 승인용");
+		$objPHPExcel->getProperties()->setSubject("페기장비 목록 - 승인용");
+		$objPHPExcel->getProperties()->setDescription("페기장비 목록 - 승인용, generated using PHP classes.");
+
+		$headers = array('타입', 'Serial Number', '장비종류', '모델명', '상태', '수량', '직전점포', '등록일');
+
+		// 헤더 설정
+		$nCol = 'A';
+		$nRow = 1;
+		$objPHPExcel->setActiveSheetIndex(0);
+		foreach($headers as $h) {
+			$objPHPExcel->getActiveSheet()->SetCellValue($nCol . $nRow, $h);
+			$nCol++;
+		}
+
+		// 데이터 채우기
+		$this->load->model('work_m');
+		$op = $this->work_m->get(24);
+
+		$rows = $op->getItems();
+		
+		foreach($rows as $row) {
+			$nRow++;
+			$prev_data = ($row->prev_location) ? gs2_decode_location($row->prev_location)->name : '';
+			
+			$objPHPExcel->getActiveSheet()->SetCellValue('A'.$nRow, gs2_part_type($row->part_type));
+			$objPHPExcel->getActiveSheet()->SetCellValue('B'.$nRow, $row->serial_number);
+			$objPHPExcel->getActiveSheet()->SetCellValue('C'.$nRow, $row->part->category->name);
+			$objPHPExcel->getActiveSheet()->SetCellValue('D'.$nRow, $row->part->name);
+			$objPHPExcel->getActiveSheet()->SetCellValue('E'.$nRow, '중고');
+			$objPHPExcel->getActiveSheet()->SetCellValue('F'.$nRow, $row->qty_request);
+			$objPHPExcel->getActiveSheet()->SetCellValue('G'.$nRow, $prev_data);
+			$objPHPExcel->getActiveSheet()->SetCellValue('H'.$nRow, date("Y-m-d"));
+		}
+
+		// 타이틀
+		$objPHPExcel->getActiveSheet()->setTitle('폐기장비목록');
+		$save_filename = 'test_' . date('Ymd') . '.xls';
+		
+		// 엑셀파일 다운로드
+		$this->download($objPHPExcel, $save_filename);		
 	}
 
 	// 예제 1
@@ -26,8 +92,6 @@ class Myexcel extends CI_Controller {
 		$objPHPExcel->getProperties()->setSubject("Office 2007 XLSX Test Document");
 		$objPHPExcel->getProperties()->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.");
 
-
-		// Add some data
 		// echo date('H:i:s') . " Add some data\n";
 		$objPHPExcel->setActiveSheetIndex(0);
 		$objPHPExcel->getActiveSheet()->SetCellValue('A1', 'Hello');
@@ -47,19 +111,6 @@ class Myexcel extends CI_Controller {
 
 		// echo date('H:i:s') . " Done writing file.\r\n";
 
-		$save_filename = 'test_' . date('Ymd') . '.xls';
-		// redirect output to client browser
-		header("Pragma: public");
-		header("Expires: 0");
-		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-		header("Content-Type: application/octet-stream");
-		header('Content-Type: application/vnd.ms-excel');
-		header('Content-Disposition: attachment;filename="' .$save_filename. '"');
-		header('Cache-Control: max-age=0');
-
-		$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-		$objWriter->setOffice2003Compatibility(true);
-		$objWriter->save('php://output');
 	}
 
 
