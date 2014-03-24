d41bbbed2b099f68ecfc632b3b0885b2ab72ea79
광희 장 <nogovern@jangkh-macbook.local>
2014-03-24 17:25:12 +0900

    수리 - 컨트롤러 및 리스트 view 파일 추가

---
+++ application/controllers/work/repair.php
@@ -0,0 +1,144 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* 	수리 컨트롤러
+*/
+class Repair extends CI_Controller
+{
+	
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->model('work_m', 'work_model');
+	}
+
+	public function index() {
+		$this->lists();
+	}
+
+	public function lists($page = 1) {
+
+		$this->load->helper('form');
+
+		$data['title'] = '수리 업무';
+		$data['current'] = 'page-repair';
+
+		///////////////
+		// 검색 조건
+		///////////////
+		$criteria = array();
+		
+		// 상태
+		if($this->input->get('status')) {
+			$criteria['status'] = $this->input->get('status');
+		}
+
+		// 형태
+		if($this->input->get('type')) {
+			$criteria['type'] = $this->input->get('type');
+		}
+
+		// 사무소 - GET 유무 확인시 없을떄 false 로 타입까지 비교해야 함
+		if($this->input->get('off_id') === false) {
+			$criteria['office'] = (gs2_user_type() == '1') ? $this->session->userdata('office_id') : 0;
+		} else {
+			$criteria['office'] = $this->input->get('off_id');
+		}
+
+		// pagination 초기화
+		$config = $this->work_model->setPaginationConfig('work/repair/lists/');
+
+		$data['rows'] = $this->work_model->getOperations(GS2_OP_TYPE_REPAIR, $criteria, GS2_LIST_PER_PAGE, $page);
+		// 총 결과수
+		$total_rows = $this->work_model->numRows(GS2_OP_TYPE_REPAIR, $criteria);
+		$config['total_rows'] = $total_rows;
+
+		$this->pagination->initialize($config);
+		$data['pagination'] = $this->pagination->create_links();
+		$data['total_rows'] = $total_rows;
+
+		// ===============
+		//  필터링 데이터
+		// ===============
+		$this->load->helper('form');
+
+		// 진행상태
+		$data['status_filter'] = form_dropdown('status', gs2_op_status_list('2'), $this->input->get('status'), 'id="status_filter" class="form-control"');
+
+		// 작업형태
+		$type_list = array(
+			'0'	=> '-- 전체 --',	
+			'801'	=> '수리-입고',	
+			'802'	=> '수리-출고',	
+		);
+		
+		$data['type_filter'] = form_dropdown('type', $type_list, $this->input->get('type'), 'id="type_filter" class="form-control"');
+
+		// 담당 사무소
+		$this->load->model('office_m', 'office_model');
+		$arr_office = gs2_convert_for_dropdown($this->office_model->getList());
+		$arr_office['0'] = '--전체--';
+		$data['office_filter'] = form_dropdown('off_id', $arr_office, $criteria['office'], 'id="office_filter" class="form-control"');
+
+		/////////////////////////// modal 요청서 용 ////////
+		
+		// 사무소 select 생성
+		$data['select_office'] = form_dropdown('select_office', $arr_office, $this->session->userdata('office_id'), 'id="select_office" class="form-control required" disabled="disabled" ');
+
+		// 업체 선택
+		$this->load->model('company_m', 'company_model');
+		$companies = gs2_convert_for_dropdown($this->company_model->getTransferClients());
+		$data['select_company'] = form_dropdown('select_company', $companies, 0, 'id="select_company" class="form-control"');
+		
+		$this->load->view('work/work_repair_list', $data);
+
+	}
+
+	public function view($id) {
+
+		$data['title'] = "수리 업무 상세 보기";
+		$data['current'] = 'page-repair';
+		$data['_config'] = $this->config->item('gs2');
+
+		$work = $this->work_model->get($id);
+		$data['op'] = $work;
+
+		// 장비 카테고리 dropdown
+		$this->load->helper('form');
+		$this->load->model('category_m', 'category_model');
+		$cats = $this->category_model->getValidPartCategories();
+		$cats = gs2_convert_for_dropdown($cats);
+		$data['select_category'] = form_dropdown('select_category', $cats, 0, 'id="select_category" class="form-control"');
+
+		$this->load->view('work/work_repair_view', $data);
+	}
+
+	public function register() {
+		// gs2_dump($this->input->post());
+
+		$post_data['office_id'] = $this->session->userdata('office_id');
+
+		$post_data['op_type'] =  $this->input->post('op_type');
+		$post_data['company_id'] = $this->input->post('select_company');
+		$post_data['date_request'] = '';
+
+		// 업무 생성
+		$op = $this->work_model->createOperation( $this->input->post('op_type'), $post_data);
+
+		// 로그 기록
+		$log_data = array(
+			'type'		=> '1',
+			'content'	=> '수리 업무가 생성되었음',
+			'event'		=> '생성'
+		);
+		$this->work_model->addLog($op, $log_data, TRUE);
+
+		redirect('work/repair');
+	}
+
+	public function delete($id) {
+		
+	}
+
+}
---
+++ application/views/work/work_repair_list.php
@@ -0,0 +1,218 @@
+<?php
+$this->view('layout/header');
+$this->view('layout/navbar');
+?>
+<!-- start of div.container -->
+<div class="container">
+  <!-- Main hero unit for a primary marketing message or call to action -->
+  <div class="page-header">
+    <h1><span class="fa fa-desktop"></span>&nbsp;수리 업무</h1>
+  </div>
+ 
+  <!-- Example row of columns -->
+  <div class="row">
+    <div class="col-md-12">
+      
+      <!-- filter -->
+      <div class="well well-sm">
+        <form method="post" id="filter-form" class="form-inline" role="search">
+          <input type="hidden" name="csrf_test_name" value="<?php echo $this->security->get_csrf_hash();?>">
+          <div class="form-group">
+            진행상태 : 
+            <?php echo $status_filter; ?>
+          </div>
+
+          <div class="form-group">
+            &nbsp;&nbsp;작업형태 : 
+            <?php echo $type_filter; ?>
+          </div>
+
+          <div class="form-group">
+            &nbsp;&nbsp;사무소:
+            <?php echo $office_filter; ?>
+          </div>
+
+          <div class="form-group">
+            &nbsp;&nbsp; 
+            <button type="submit" class="btn btn-primary btn-sm">검색</button> 
+          </div>
+        </form>
+      </div>
+
+      <table id="op_list" class="table table-hover table-condensed table-responsive">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>작업구분</th>
+            <th>요청자</th>
+            <th>승인/수리 업체</th>
+            <th>요청/출고 수량</th>
+            <th>재고사무소</th>
+            <th>상태</th>
+            <th>등록일</th>
+            <th>완료일</th>
+            <th>&nbsp;</th>
+          </tr>
+        </thead>
+
+        <tbody>
+<?php
+foreach($rows as $row):
+  switch($row->status) {
+    case '1': $label_color = 'label-default';break;
+    case '2': $label_color = 'label-warning';break;
+    case '3': $label_color = 'label-success';break;
+    default : $label_color = 'label-default';break;
+  }
+
+  if( $row->type == '601') {
+    $company_name = 'GS';
+  } else {
+    $company_name = gs2_location_name($row->work_location);
+  }
+
+?>
+          <tr class="">
+            <td><?=$row->id?></td>
+            <td><?=gs2_op_type($row->type)?></td>
+            <td><?=$row->user->name?></td>
+            <td><?=$company_name?></td>
+            <td><?=$row->getTotalRequestQty()?></td>
+            <td><?=$row->office->name?></td>
+            <td>
+              <span class="label <?=$label_color?>"><?=constant("GS2_OP_MOVE_STATUS_" .$row->status)?></span>
+            </td>
+            <td><?=$row->getDateRegister();?></td>
+            <td><?=$row->getDateFinish();?></td>
+            <td><button class="btn btn-default btn-sm btn_view" type="button" data-href="<?=site_url('work/destroy/view/') . '/' . $row->id ?>">보기</button></td>
+          </tr>
+<?php
+endforeach;
+?>
+        </tbody>
+      </table>
+      <!-- Pagination -->
+      <div style="text-align:center">
+        <?=$pagination?>
+      </div>
+
+      <p>
+        <button id="btn_request_destroy" type="button" class="btn btn-primary"><i class="fa fa-pencil-square-o"></i>&nbsp;요청서 등록</button>
+      </p>
+
+    </div>
+  </div>
+</div><!-- end of container -->
+
+<!-- modal dialog -->
+<div class="modal fade" id="modal_request_destroy" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">수리 요청서</h4>
+      </div>
+      <!-- start form -->
+      <form role="form" class="form form-horizontal" method="post" action="<?php echo site_url('work/destroy/register');?>">
+        <input type="hidden" name="csrf_test_name" value="<?php echo $this->security->get_csrf_hash();?>">
+        <div class="modal-body">
+          <div class="well well-sm">
+            <span class="text-danger">장비 수리 요청을 등록합니다.</span>
+          </div>
+          
+          <div class="form-group">
+            <label class="form-label col-sm-3">형태</label>
+            <div class="input-group col-sm-6">
+              <select name="op_type" id="op_type" class="form-control">
+                <option value="0">--선택하세요--</option>
+                <option value="501">출고</option>
+                <option value="502">입고</option>
+              </select>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label class="form-label col-sm-3">재고 사무소</label>
+            <div class="input-group col-sm-6">
+              <?php echo $select_office ?>
+            </div>
+          </div>
+
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-primary">완료</button>
+          <button type="button" class="btn btn-default" data-dismiss="modal">취소</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div><!-- /.modal -->
+
+
+<script type="text/javascript">
+
+// notEqual validate rule 추가
+jQuery.validator.addMethod("notEqual", function(value, element, param) {
+  return this.optional(element) || value != param;
+}, "송신사무소와 수신사무소와 같을 수 없습니다");
+
+$(document).ready(function(){
+  // open modal
+  $("#btn_request_destroy").click(function(){
+    $("#modal_request_destroy").modal('show');
+  });  
+
+  // 상세 보기 페이지로 이동
+  $("button.btn_view").click(function(){
+    var href = $(this).data('href');
+    location.href = href;
+    return false;
+  });
+
+  $("#modal_request_destroy form").validate({
+    rules: {
+      select_office: {
+        required: true,
+        min: 1
+      },
+      op_type: {
+        required: true,
+        min: 1,
+      }
+    },
+    messages: {
+      select_office: {
+        min: '재고 사무소를 선택하세요'
+      },
+      op_type: {
+        min: '수리업무 형태를 선택하세요'
+      }
+    },
+    submitHandler: function(form) {
+      form.submit();
+    }
+
+  });
+
+  /////////////////////////
+  // bootstrap 3 popover //
+  /////////////////////////
+  $(".popover_memo").popover({trigger: 'hover', placement: 'left'});
+  $(".popover").click(function(e){e.preventDefault();});
+
+  ///////////////////////
+  // 검색 필터 전송 
+  ///////////////////////
+  $("#filter-form").submit(function() {
+    var url = _base_url + 'work/destroy/?';
+    var query = $(this).serialize();
+    
+    $(this).prop('action', url + query);
+  });
+
+});
+</script>
+
+<?php
+$this->view('layout/footer');
+?>
